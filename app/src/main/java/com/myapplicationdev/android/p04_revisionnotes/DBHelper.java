ea2883196f25a78620dc75266a22484b1015731b
MityaChang <qikai1998@gmail.com>
2021-05-04 13:54:35 +0800

    Done DBHelper

--- app/src/main/java/com/myapplicationdev/android/p04_revisionnotes/DBHelper.java
+++ app/src/main/java/com/myapplicationdev/android/p04_revisionnotes/DBHelper.java
@@ -11,39 +11,81 @@ import java.util.ArrayList;
 
 public class DBHelper extends SQLiteOpenHelper {
 
-	//TODO Define the Database properties
-	private static final String DATABASE_NAME = "";
-	private static final int DATABASE_VERSION = ;
+    //TODO Define the Database properties
+    private static final String DATABASE_NAME = "";
+    private static final int DATABASE_VERSION = 1;
+
+    private static final String TABLE_NOTE = "Note";
+    private static final String COLUMN_ID = "_id";
+    private static final String COLUMN_DESCRIPTION = "noteContent";
+    private static final String COLUMN_IMG = "stars";
 
 
-	public DBHelper(Context context) {
-		super(context, DATABASE_NAME, null, DATABASE_VERSION);
-	}
+    public DBHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
 
-	@Override
-	public void onCreate(SQLiteDatabase db) {
-		//TODO CREATE TABLE Note
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        //TODO CREATE TABLE Note
+		String createTableSql = "CREATE TABLE " + TABLE_NOTE +  "("
+				+ COLUMN_ID + " INTEGER PRIMARY KEY AUTOINCREMENT,"
+				+ COLUMN_IMG + " TEXT,"
+				+ COLUMN_DESCRIPTION + " TEXT )";
+		db.execSQL(createTableSql);
+        Log.i("info", "created tables");
+    }
 
-	}
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_NOTE);
+        onCreate(db);
+    }
 
-	@Override
-	public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-		db.execSQL("DROP TABLE IF EXISTS " + TABLE_NOTE);
-		onCreate(db);
-	}
+    public void insertNote(String noteContent, int stars) {
+        //TODO insert the data into the database
+        SQLiteDatabase db = this.getWritableDatabase();
+        // We use ContentValues object to store the values for
+        //  the db operation
+        ContentValues values = new ContentValues();
+        // Store the column name as key and the description as value
+        values.put(COLUMN_DESCRIPTION, noteContent);
+        values.put(COLUMN_IMG, (Integer) stars);
+        // Insert the row into the TABLE_TASK
+        db.insert(TABLE_NOTE, null, values);
+        // Close the database connection
+        db.close();
+    }
 
-	public void insertNote(String noteContent, int stars) {
-		//TODO insert the data into the database
-	}
+    public ArrayList<Note> getAllNotes() {
+        //TODO return records in Java objects
+        ArrayList<Note> tasks = new ArrayList<Note>();
+        String selectQuery = "SELECT " + COLUMN_ID + ", "
+                + COLUMN_DESCRIPTION + ", "
+                + COLUMN_IMG
+                + " FROM " + TABLE_NOTE;
 
-	public ArrayList<Note> getAllNotes() {
-		//TODO return records in Java objects
-	}
+        SQLiteDatabase db = this.getReadableDatabase();
+        Cursor cursor = db.rawQuery(selectQuery, null);
+
+        if (cursor.moveToFirst()) {
+            do {
+                int id = cursor.getInt(0);
+                String description = cursor.getString(1);
+                int date = cursor.getInt(2);
+                Note obj = new Note(id, description, date);
+                tasks.add(obj);
+            } while (cursor.moveToNext());
+        }
+        cursor.close();
+        db.close();
+        return tasks;
+    }
 
     public ArrayList<String> getNoteContent() {
         //TODO return records in Strings
 
-		// Create an ArrayList that holds String objects
+        // Create an ArrayList that holds String objects
         ArrayList<String> notes = new ArrayList<String>();
         // Select all the notes' content
         String selectQuery = "";
@@ -57,7 +99,11 @@ public class DBHelper extends SQLiteOpenHelper {
             // Loop while moveToNext() points to next row and returns true;
             // moveToNext() returns false when no more next row to move to
             do {
-
+                int id = cursor.getInt(0);
+                String description = cursor.getString(1);
+                int star = cursor.getInt(2);
+                Note obj = new Note(id, description, star);
+                notes.add(String.valueOf(obj));
 
             } while (cursor.moveToNext());
         }
